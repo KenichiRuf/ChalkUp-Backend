e2e7072acc89786327131f58548b7c1f682d7f25
HaBuDeSu <ruf.kenichi@gmail.com>
2020-01-02 21:29:43 -0600

    change production settings in knexfile to connect to heroku postgres

--- api/server.js
+++ api/server.js
@@ -3,11 +3,11 @@ const server = express();
 const helmet = require("helmet");
 const cors = require("cors");
 
-const routeRouter = require("./routes/routeRouter");
+//const routeRouter = require("./routes/routeRouter");
 const authRouter = require("./auth/authRouter");
 
 server.use("/api/auth", authRouter);
-server.use("/api/routes", routeRouter);
+//server.use("/api/routes", routeRouter);
 
 server.get("/", (req, res) => {
   res.send("Backend API for ChalkUp");
