b0dc2381079fd1dedc5e065be6f6e7b00fc9074d
HaBuDeSu <ruf.kenichi@gmail.com>
2020-01-02 20:51:20 -0600

    authRouter

--- api/server.js
+++ api/server.js
@@ -3,6 +3,12 @@ const server = express();
 const helmet = require("helmet");
 const cors = require("cors");
 
+const routeRouter = require("./routes/routeRouter");
+const authRouter = require("./auth/authRouter");
+
+server.use("/api/auth", authRouter);
+server.use("/api/routes", routeRouter);
+
 server.get("/", (req, res) => {
   res.send("Backend API for ChalkUp");
 });
